378eee20d7c4dcd9a33abbb753e61438729580e2
Thatohatsi Tsoho <marciatsoho@gmail.com>
2021-11-23 13:57:11 +0200

    app

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const flash = require('express-flash');
+const avoDeals = require('./avo-shopper');
+const postgres = require('pg');
+const Pool = postgres.Pool;
 
 const app = express();
-const PORT =  process.env.PORT || 3019;
+
+let useSSL = false;
+let local = process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0" || false;
+if (process.env.DATABASE_URL && !local) {
+    useSSL = true;
+}
+
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/avocado';
+
+const pool = new Pool({
+    connectionString,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+});
+
+const avos = avoDeals(pool)
+
+// const handlebarSetup = exphbs({
+// 	partialsDir: "./views/partials",
+// 	viewPath: "./views",
+// 	layoutsDir: "./views/layouts",
+//   });
+
+  app.use(session({
+	secret : "error messages",
+	resave: false,
+	saveUninitialized: true,
+  }));
+
+  app.use(flash())
 
 // enable the req.body object - to allow us to use HTML forms
 app.use(express.json());
@@ -14,7 +50,11 @@ app.use(express.static('public'));
 // add more middleware to allow for templating support
 
 app.engine('handlebars', exphbs.engine());
+// app.engine('handlebars', handlebarSetup);
 app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 let counter = 0;
 
@@ -25,6 +65,7 @@ app.get('/', function(req, res) {
 });
 
 // start  the server and start listening for HTTP request on the PORT number specified...
+let PORT = process.env.PORT || 1100;
 app.listen(PORT, function() {
 	console.log(`AvoApp started on port ${PORT}`)
 });
